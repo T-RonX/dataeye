14050047e1b47a892b68802978a57db926aeb695
Ron <ron.gahler@wolterskluwer.com>
2023-08-12 23:33:51 +0200

    Return immutable date time by default

--- src/DateTimeProvider/DateTimeProvider.php
+++ src/DateTimeProvider/DateTimeProvider.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace App\DateTimeProvider;
 
 use Carbon\Carbon;
+use Carbon\CarbonImmutable;
 use DateTime;
+use DateTimeImmutable;
 
 class DateTimeProvider
 {
-    public function getNow(): DateTime
+    public function getNow(): DateTimeImmutable
+    {
+        return CarbonImmutable::now();
+    }
+
+    public function getNowMutable(): DateTime
     {
         return Carbon::now();
     }
